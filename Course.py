dcd5123099d996128a1abcd01bc90b2029a1d0ba
杨舒博 <yangshubo@DN525fb2.SUNet>
2022-11-07 17:51:30 -0800

    Consolidated all the classes

--- Course.py
+++ Course.py
@@ -1,7 +1,3 @@
-from typing import Dict, Set, Tuple
-from CourseScheduler import State
-
-
 class Course:
     # TODO complete it
     """_summary_
@@ -18,10 +14,10 @@ class Course:
     def __init__(
         self,
         reward: float,
-        units: Tuple[int],
+        units: tuple,
         course_number: str,
         course_name: str,
-        quarter_indices: Tuple[int],
+        quarter_indices: tuple,
     ) -> None:
 
         self.reward = reward
@@ -29,103 +25,3 @@ class Course:
         self.course_number = course_number
         self.course_name = course_name
         self.quarter_indices = quarter_indices
-
-
-class ExploreCourse:
-    def __init__(
-        self, class_database: Dict[int, Set], course_category: Dict[str, Set]
-    ) -> None:
-        """_summary_
-
-        Args:
-            class_database (Dict[int, Set]): _description_
-            course_category (Dict[str, Set]): _description_
-        """
-        self.class_database = class_database
-        self.course_category = course_category
-
-    def get_actions(
-        self, quarter_index: int, remaining_units: int
-    ) -> Set[Tuple[str, int]]:
-        """_summary_
-
-        Args:
-            quarter_index (int): _description_
-            remaining_units (int): _description_
-
-        Raises:
-            Exception: _description_
-
-        Returns:
-            Set[Tuple[str, int]]: _description_
-        """
-
-        raise Exception("Not Implemented yet")
-
-    def get_quarter_cost(self, enrolled_courses: Set[Tuple[str, int]]) -> float:
-        """_summary_
-
-        Args:
-            enrolled_courses (Set[Tuple[str, int]]): _description_
-
-        Raises:
-            Exception: _description_
-
-        Returns:
-            float: _description_
-        """
-
-        raise Exception("Not implemented yet")
-
-
-class FindCourses:
-    """_summary_
-
-    Args:
-        explore_course (ExploreCourse): _description_
-    """
-
-    def __init__(self, explore_course: ExploreCourse) -> None:
-        self.explore_course = explore_course
-
-    def start_state(self, remaining_units: int) -> State:
-        """_summary_
-
-        Args:
-            remaining_units (int): _description_
-
-        Raises:
-            Exception: _description_
-
-        Returns:
-            State: _description_
-        """
-        raise Exception("Not Implemented yet")
-
-    def is_end(self, state: State) -> bool:
-        """_summary_
-
-        Args:
-            state (State): _description_
-
-        Raises:
-            Exception: _description_
-
-        Returns:
-            bool: _description_
-        """
-        raise Exception("Not Implemented yet")
-
-    def successors_and_cost(self, state: State) -> Tuple[State, float]:
-        """_summary_
-
-        Args:
-            state (State): _description_
-
-        Raises:
-            Exception: _description_
-
-        Returns:
-            Tuple[State, float]: _description_
-        """
-        raise Exception("Not Implemented yet")
--- SearchProblem.py
+++ SearchProblem.py
@@ -1,5 +1,5 @@
 from typing import Tuple, List, Any
-from CourseScheduler import State
+from State import State
 
 
 class SearchProblem:
@@ -45,23 +45,3 @@ class SearchProblem:
             List[Tuple]: _description_
         """
         raise NotImplementedError("Override me")
-
-
-class UniformCostSearch:
-    """_summary_
-
-    Args:
-        verbose (int, optional): _description_. Defaults to 0.
-    """
-
-    def __init__(self, verbose: int = 0) -> None:
-        self.verbose = verbose
-
-    def solve(self, problem: SearchProblem) -> None:
-        """_summary_
-
-        Args:
-            problem (SearchProblem): _description_
-        """
-
-        raise Exception("Not Implemented yet")
