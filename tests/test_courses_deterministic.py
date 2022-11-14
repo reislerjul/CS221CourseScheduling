8a08b18c2dc0f2c95093ac83c895ab045c126dfb
reginazhai <wanyuezhai@gmail.com>
2022-11-14 01:08:38 -0800

    update on courses structure

--- tests/test_courses_deterministic.py
+++ tests/test_courses_deterministic.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..CoursesDeterministic import CoursesDeterministic
+from ..courses_deterministic import CoursesDeterministic
 
 
 COURSE_YEARS = ["2021-2022", "2022-2023", "2023-2024"]
@@ -10,8 +10,8 @@ COURSE_YEARS = ["2021-2022", "2022-2023", "2023-2024"]
 def test_courses_deterministic_init(course_year, mocker):
 
     # Mock course_extraction since this takes a long time
-    mocker.patch.object(CoursesDeterministic, "course_extraction", return_value=None)
-    courses_deterministic = CoursesDeterministic(course_year)
+    mocker.patch.object(CoursesDeterministic, "run", return_value=None)
+    courses_deterministic = CoursesDeterministic([course_year])
 
-    message = f"Year is not set correctly in CoursesDeterministic! Expected {course_year}, got {courses_deterministic.year}."
-    assert courses_deterministic.year == course_year, message
+    message = f"Year not set correctly in CoursesDeterministic! Expected {[course_year]}, got {courses_deterministic.years}."
+    assert courses_deterministic.years == [course_year], message
