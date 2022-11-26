c0ce6da549d7711e742413e182d43ae032762e70
reginazhai <wanyuezhai@gmail.com>
2022-11-25 22:46:41 -0800

    Update on Search with A LOT of errors

--- src/course_scheduler.py
+++ src/course_scheduler.py
@@ -8,24 +8,25 @@ class State:
         current_quarter (int): _description_
         course_taken (Set): _description_
         remaining_units (Dict[str, int]): _description_
+        remaining_instructors (List): _description_
+        remaining_research (int): _description_
     """
 
     def __init__(
-        self, current_quarter: int, course_taken: List, remaining_units: Dict[str, int]
+        self,
+        current_quarter: int,
+        course_taken: List,
+        remaining_units: Dict[str, int],
+        # Remaining desired professors
+        remaining_instructors: List,
+        # Remaining mandatory research units
+        remaining_resesarch: int,
     ) -> None:
         self.current_quarter = current_quarter
         self.course_taken = course_taken
         self.remaining_units = remaining_units
-
-        # TODO: Add waypoints for specific professors
-        #
-        #
-        #
-
-        # TODO: Add waypoints for terms to do research
-        #
-        #
-        #
+        self.remaining_instructors = remaining_instructors
+        self.remaining_research = remaining_resesarch
 
     def print_state(self):
         print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
--- src/search_problem.py
+++ src/search_problem.py
@@ -17,6 +17,11 @@ class FindCourses:
         self,
         explore_course: ExploreCourse,
         units_requirement: Dict[str, int],
+        # Newly added instructors, research_units, internship and research_terms
+        instructors: List,
+        research_units: int,
+        internship: bool,
+        research_terms: List,
         max_quarter: int,
         max_successors: int,
     ) -> None:
@@ -29,8 +34,18 @@ class FindCourses:
         """
         self.explore_course = explore_course
         self.units_requirement = units_requirement
+        self.instructors = instructors
+        self.research_units = research_units
+        self.internship = internship
+        self.research_terms = research_terms
         self.max_quarter = max_quarter
         self.max_successors = max_successors
+
+    # TODO: move utility function to a better place
+    def research_class(self, course):
+        if ("research" in course.course_name) or ("Research" in course.course_name):
+            return True
+        return False
 
     def _get_actions(self, state: State) -> List[List[Tuple[Course, int]]]:
         """_summary_
@@ -50,10 +65,10 @@ class FindCourses:
         Returns:
             Set[Course]: _description_
         """
-        # TODO: Filter out the quarter if internship = True
-        #
-        #
-        #
+        # Filter out the quarter if planning to do internship during the summer
+        nxt_quarter = state.current_quarter + 1
+        if self.internship and (nxt_quarter == 4 or nxt_quarter == 8):
+            return []
 
         # Offered in the next quarter
         courses_offered = self.explore_course.class_database[state.current_quarter + 1]
@@ -87,16 +102,21 @@ class FindCourses:
         ]
 
         # Combinations
-        # TODO: relax the number of courses available for each quarter
-        #
-        #
-        #
+        # Relax the number of courses available for each quarter
+        # TODO: decide on what to include for the quarters of courses
+
         combins = combinations(candidate_courses, 2)
         actions = []
 
         for combin in combins:
             course1 = combin[0]
             course2 = combin[1]
+            # Make sure to include research credits in appropriate quarter
+            if nxt_quarter in self.research_terms:
+                if (not self.research_class(course1)) and (
+                    not self.research_class(course2)
+                ):
+                    return []
 
             for units1 in range(course1.units[0], course1.units[1] + 1):
                 for units2 in range(course2.units[0], course2.units[1] + 1):
@@ -105,11 +125,14 @@ class FindCourses:
                         units1 + units2 >= MIN_UNITS_PER_QUARTER
                         and units1 + units2 <= MAX_UNITS_PER_QUARTER
                     ):
+
                         actions.append([(course1, units1), (course2, units2)])
 
         return actions
 
-    def _get_quarter_cost(self, enrolled_courses: List[Tuple[Course, int]]) -> float:
+    def _get_quarter_cost(
+        self, enrolled_courses: List[Tuple[Course, int]], state: State, prereqs: List
+    ) -> float:
         """_summary_
 
         Args:
@@ -130,13 +153,19 @@ class FindCourses:
             total_units += course_and_unit[1]
             total_rewards += course_and_unit[1] * course_and_unit[0].reward
 
-        # TODO: add soft requirement for previously taken courses
+        # Add soft requirement for previously taken courses
         # Give higher reward for completed prerequisites
         # Give lower reward for non-completed prerequisites
-        # Note: UCS cannot have cost below 0
-        #
-        #
-        #
+        # TODO: figure out how to include the prerequisites of the class
+
+        prereq_count = 0
+        for i in range(len(prereqs)):
+            if prereqs[i] in state.course_taken:
+                prereq_count += 1
+        # Current logic:
+        # Reward proportional to the number of prerequisites satisfied
+        # Maximum of 5 total rewards possible
+        total_rewards += 5 / len(prereqs) * prereq_count
 
         # TODO: add time component for course cost
         # Assign higher cost for courses larger than 300
@@ -144,7 +173,9 @@ class FindCourses:
         #
         #
         #
-        return total_units * MAX_CLASS_REWARD - total_rewards
+
+        # Note: UCS cannot have cost below 0
+        return max(total_units * MAX_CLASS_REWARD - total_rewards, 0)
 
     def start_state(self) -> State:
         """_summary_
@@ -155,7 +186,9 @@ class FindCourses:
         Returns:
             State: _description_
         """
-        return State(0, [], self.units_requirement)
+        return State(
+            0, [], self.units_requirement, self.instructors, self.research_units
+        )
 
     def is_end(self, state: State) -> bool:
         """_summary_
@@ -169,11 +202,12 @@ class FindCourses:
         Returns:
             bool: _description_
         """
-        # TODO: include the case where a single class satisfies multiple requirements
-        #
-        #
-        #
-        if sum(state.remaining_units.values()) <= 0:
+
+        if (
+            (state.remaining_instructors == [])
+            and (state.remaining_research == 0)
+            and sum(state.remaining_units.values()) <= 0
+        ):
             return True
         else:
             return False
@@ -201,19 +235,32 @@ class FindCourses:
         for action in actions:
             suc_current_quarter = state.current_quarter + 1
             suc_remaining_units = copy.deepcopy(state.remaining_units)
+            suc_remaining_instructors = copy.deepcopy(state.remaining_instructors)
+            suc_remaining_research = copy.deepcopy(state.remaining_research)
 
             courses_this_quarter = []
             for course, units in action:
+                # TODO: include the case where a single class satisfies multiple requirements
                 suc_remaining_units[course.course_category] -= units
+                if self.research_class(course):
+                    suc_remaining_research -= units
+                if course.instructor in suc_remaining_instructors:
+                    suc_remaining_instructors.remove(course.instructor)
                 courses_this_quarter.append(course)
 
             suc_courses_taken = state.course_taken + courses_this_quarter
-            suc_cost = self._get_quarter_cost(action)
+            suc_cost = self._get_quarter_cost(action, state, [])
 
             successors.append(
                 (
                     action,
-                    State(suc_current_quarter, suc_courses_taken, suc_remaining_units),
+                    State(
+                        suc_current_quarter,
+                        suc_courses_taken,
+                        suc_remaining_units,
+                        suc_remaining_instructors,
+                        suc_remaining_research,
+                    ),
                     suc_cost,
                 )
             )
@@ -227,6 +274,8 @@ class FindCourses:
                         state.current_quarter + 1,
                         copy.deepcopy(state.course_taken),
                         copy.deepcopy(state.remaining_units),
+                        copy.deepcopy(state.remaining_instructors),
+                        copy.deepcopy(state.remaining_research),
                     ),
                     1000,
                 )
