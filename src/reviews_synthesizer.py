92209c9cc56eebe11b639b04eed1e1b529c39a25
prushto <100314507+prushto@users.noreply.github.com>
2022-11-27 14:00:16 -0800

    Updated reviews_synthesizer to incorporate comments

--- src/reviews_synthesizer.py
+++ src/reviews_synthesizer.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import random
 
+SENTIMENT_ANALYSIS = False
+
 
 class ReviewSynthesizer:
     """
@@ -15,13 +17,11 @@ class ReviewSynthesizer:
     # TODO: THIS FILE IS WORK IN PROGRESS! See points below
     """
     Needs to be integrated into courses_deterministic. 2 design questions to answer:
-    1. do we want this class to read Stanford from a csv? Seems may be more efficient if it can take
-    Stanford course list as an array.
-    2. where do we want to store the courses with reviews? It makes sense to keep it as a separate dataframe,
-    and I suggest we store that dataframe as an instance variable of CoursesDeterministic.
-    3. I am just using a uniform random allocation of coursera courses to Stanford courses. We may want to do
-    something else. Also, we may want to filter the coursera courses for number of reviews or some other
-    proxy for quality.
+    1. do we want this class to read Stanford from a csv? Seems may be more efficient if this class can take
+    Stanford course list as an array from courses_deterministic.
+    2. I am just using a uniform random allocation of Coursera courses to Stanford courses. We may want to do
+    something else. Also, we may want to filter the Coursera courses for number of reviews or some other
+    proxy for quality. Some Coursera courses may only have 1 or 2 reviews associated with them.
     """
 
     def __init__(
@@ -29,30 +29,45 @@ class ReviewSynthesizer:
         course_list_loc="../data/2022-2023_CS.csv",  # NEEDS FIXING WHEN INTEGRATED INTO COURSES_DETERMINISTIC
         coursera_file_loc="../data/coursera_reviews_by_course.csv",
     ):
-        course_list = pd.read_csv(course_list_loc)
-        course_numbers = course_list["course_number"]
-        coursera_reviews = pd.read_csv(coursera_file_loc)
+        self.course_list_loc = course_list_loc
+        self.coursera_file_loc = coursera_file_loc
+        self.courses_with_reviews = pd.DataFrame()
+        self.course_numbers = []
+        self.course_syn_scores = {}
+
+    def synthesize_reviews(self):
+        course_list = pd.read_csv(self.course_list_loc)
+        self.course_numbers = course_list["course_number"]
+        coursera_reviews = pd.read_csv(self.coursera_file_loc)
         unique_coursera = coursera_reviews.CourseId.unique()
-        courses_with_reviews = pd.DataFrame(
-            columns=["course_number", "review", "label"]
-        )
-        for i in range(len(course_numbers)):
-            selection = random.choice(
-                unique_coursera
-            )  # note this is with replacement, which we may not want
+        courses_with_reviews = pd.DataFrame()
+        for i in range(len(self.course_numbers)):
+            selection = random.choice(unique_coursera)
             temp = coursera_reviews.loc[coursera_reviews["CourseId"] == selection]
             temp = temp.drop("CourseId", axis=1)
-            temp.insert(loc=0, column="course_number", value=course_numbers[i])
+            temp.insert(loc=0, column="course_number", value=self.course_numbers[i])
             courses_with_reviews = courses_with_reviews.append(temp)
         self.courses_with_reviews = courses_with_reviews
 
-    def get_courses_with_reviews(self):
-        return self.courses_with_reviews
+    def get_synthetic_ratings(self):
+        """
 
+        Returns: dictionary self.course_syn_scores {course_num : review score} where course_num is digits of the course
+        and "review score" is a float on a 1.0 to 5.0 scale.
+        e.g. CS 229 with a review of 4.3 course_num is {229: 4.3}.
 
-def main():
-    ReviewSynthesizer()
-
-
-if __name__ == "__main__":
-    main()
+        TODO: add in sentiment analysis. Currently, review score is simply the median of the labels of the Coursera
+        reviews randomly allocated by synthesize_reviews().
+        """
+        self.synthesize_reviews()
+        if SENTIMENT_ANALYSIS:
+            raise Exception("Not implemented yet")
+        else:
+            for course_num in self.course_numbers:
+                course_num_reviews = self.courses_with_reviews.loc[
+                    self.courses_with_reviews["course_number"] == course_num
+                ]
+                self.course_syn_scores[course_num] = course_num_reviews[
+                    "Label"
+                ].median()
+            return self.course_syn_scores
